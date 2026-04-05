27cc2543df537562ede85b2281d6055f311ae9c9
Apekshik Panigrahi <apekshik@gmail.com>
2026-04-05 16:33:41 -0700

    Release 1.2.3

--- Casks/director.rb
+++ Casks/director.rb
@@ -7,8 +7,8 @@
 # Update VERSION and SHA256 after each release.
 
 cask "director" do
-  version "1.2.2"
-  sha256 "63dff4307fca4ab3989fcb577e20f9e16800927acd7f4148c41b96495152fca3"
+  version "1.2.3"
+  sha256 "28b629bb6037496a0c5a0e8bad072c0e203d37dbac04bc006bfd0960112be995"
 
   url "https://github.com/apekshik/homebrew-director/releases/download/v#{version}/TheDirector.dmg"
   name "The Director"
